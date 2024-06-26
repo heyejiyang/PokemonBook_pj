1837b95ef7bc5a4eb981724f429e42fb0a5c49ce
soeunl <soeun318@gmail.com>
2024-06-26 12:40:19 +0900

    member 패키지 작업

--- src/main/java/org/choongang/board/mappers/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package org.choongang.board.mappers;
-
-import org.choongang.board.entities.Member;
-
-public interface MemberMapper {
-    Member get(String email);
-    int exists(String email);
-    int register(Member member);
-}
