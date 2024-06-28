004fbd615be2e1c860b87135dfb722f9e3745e86
heyejiyang <xoxo99312@naver.com>
2024-06-28 11:23:45 +0900

    게시글 목록 /board/list/게시판 아이디 게시글 보기 /board/view/게시글 번호 게시글 작성 /board/write/게시판 아이디 게시글 수정 /board/update/게시글 번호

--- src/main/java/org/choongang/board/controllers/BoardController.java
+++ src/main/java/org/choongang/board/controllers/BoardController.java
@@ -3,6 +3,9 @@ package org.choongang.board.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.choongang.board.entities.Board;
+import org.choongang.board.exceptions.BoardConfigNotFoundException;
+import org.choongang.board.services.config.BoardConfigInfoService;
 import org.choongang.global.config.annotations.*;
 
 import java.util.List;
@@ -12,13 +15,16 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BoardController {
     private final HttpServletRequest request;
+    private final BoardConfigInfoService configInfoService;
 
+    //공지사항
     @GetMapping("/notice")
     public String notice(){
         request.setAttribute("addCss", List.of("notice"));
         return "board/notice";
     }
 
+    //질문과답변
     @GetMapping("/question")
     public String question(){
         request.setAttribute("addCss", List.of("question"));
@@ -30,14 +36,41 @@ public class BoardController {
         return null;
     }
 
+    //게시글 목록
     @GetMapping("/list/{bId}")
-    public String list(@PathVariable("bId") String bId){ //게시글 목록
+    public String list(@PathVariable("bId") String bId){
+        commonProcess(bId);
         return "board/list";
     }
 
+    //게시글 보기
     @GetMapping("/view/{seq}")
-    public String view(@PathVariable("seq") long seq){ //게시글 보기
+    public String view(@PathVariable("seq") long seq){
         return "board/view";
     }
 
+    //게시글 작성
+    @GetMapping("/write/{bId}")
+    public String write(@PathVariable("bId") String bId){
+        commonProcess(bId);
+        return "board/write";
+    }
+
+    //게시글 수정
+    @GetMapping("/update/{seq}")
+    public String update(@PathVariable("seq") long seq){
+
+        return "board/update";
+    }
+
+    /**
+     * 모든 요청 처리 메서드에 공통 처리 부분
+     * @param bId : 게시판 아이디가 있을 경우 게시판 설정 유지/ 게시판 아이디가 없을 경우 게시판 설정 X -> 게시판이 없음(BoardConfigNotFoundException)
+     */
+    //bId가 있을때마다 처리
+    private void commonProcess(String bId){
+        Board board = configInfoService.get(bId).orElseThrow(BoardConfigNotFoundException::new);
+
+        request.setAttribute("board", board);
+    }
 }
---
+++ src/main/webapp/WEB-INF/templates/board/list.jsp
@@ -0,0 +1,11 @@
+<%--게시판 목록
+title: 게시판 이름
+--%>
+<%@ page contentType="text/html; charset=UTF-8"%>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<%@taglib prefix="layout" tagdir="/WEB-INF/tags/layouts" %>
+
+<layout:main title="${board.BName}">
+    <h1>${board.BName}</h1>
+
+</layout:main>
