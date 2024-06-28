b4afa244d2398f475a54deb7cb3dd6c509290e62
heyejiyang <xoxo99312@naver.com>
2024-06-28 17:19:51 +0900

    게시판 보드 메인, 상단 헤더 고정 글쓰기 수정중

--- src/main/java/org/choongang/board/controllers/BoardController.java
+++ src/main/java/org/choongang/board/controllers/BoardController.java
@@ -24,17 +24,17 @@ public class BoardController {
         return "board/notice";
     }
 
-    //질문과답변
-    @GetMapping("/question")
-    public String question(){
-        request.setAttribute("addCss", List.of("question"));
-        return "board/question";
-    }
-
-    @PostMapping("/question")
-    public String questionPost() {
-        return null;
-    }
+//    //질문과답변
+//    @GetMapping("/question")
+//    public String question(){
+//        request.setAttribute("addCss", List.of("question"));
+//        return "board/question";
+//    }
+//
+//    @PostMapping("/question")
+//    public String questionPost() {
+//        return null;
+//    }
 
     //게시글 목록
     @GetMapping("/list/{bId}")
@@ -70,7 +70,7 @@ public class BoardController {
     //bId가 있을때마다 처리
     private void commonProcess(String bId){
         Board board = configInfoService.get(bId).orElseThrow(BoardConfigNotFoundException::new);
-
+        request.setAttribute("addCss", List.of("list"));
         request.setAttribute("board", board);
     }
 }
