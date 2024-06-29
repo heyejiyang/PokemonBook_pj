02fe6f1e96b7f2fe80e17ddbb842775dbd1ebe52
kth232 <kth7537@naver.com>
2024-06-29 20:27:20 +0900

    회원 정보 수정 오류 수정 완료^^

--- src/main/java/org/choongang/member/services/ModifyService.java
+++ src/main/java/org/choongang/member/services/ModifyService.java
@@ -3,6 +3,7 @@ package org.choongang.member.services;
 import lombok.RequiredArgsConstructor;
 import org.choongang.global.config.annotations.Service;
 import org.choongang.global.exceptions.AlertException;
+import org.choongang.member.MemberUtil;
 import org.choongang.member.controllers.RequestModify;
 import org.choongang.member.entities.Member;
 import org.choongang.member.mappers.MemberMapper;
@@ -16,22 +17,40 @@ import java.time.LocalDateTime;
 public class ModifyService {
     private final MemberMapper mapper;
     private final ModifyValidator validator;
+    private final MemberUtil memberUtil;
 
     public void process(RequestModify form) {
         validator.check(form);
 
+        Member member = memberUtil.getMember(); //로그인한 회원 정보
+
+        String password = form.getPassword();
+        if (password != null && !password.isBlank()) {
+            String hash = BCrypt.hashpw(password, BCrypt.gensalt(12));
+            member.setPassword(hash);
+        }
+
+        member.setEmail(form.getEmail());
+        member.setUserName(form.getUserName());
+        member.setModDt(LocalDateTime.now());
+
+        mapper.modify(member);
+
+        /*
         // 비밀번호 해시화
         String hash = BCrypt.hashpw(form.getPassword(), BCrypt.gensalt(12));
 
         // DB에 영구 저장 처리
         Member member = Member.builder()
-                .email(form.getEmail())
-                .password(hash)
-                .userName(form.getUserName())
-                .modDt(LocalDateTime.now())
-                .build();
+                            .email(form.getEmail())
+                            .password(hash)
+                            .userName(form.getUserName())
+                            .modDt(LocalDateTime.now())
+                            .build();
 
         int result = mapper.modify(member); //DB row 수
+
+        */
 
         //수정일 경우 result=0
 
