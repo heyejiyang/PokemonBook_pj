c0d6a2935d248686bc99c33204627509b75de4b5
heyejiyang <xoxo99312@naver.com>
2024-06-30 21:56:02 +0900

    게시판 기능 추가중

---
+++ src/main/java/org/choongang/board/services/write/BoardWriteInfoService.java
@@ -0,0 +1,17 @@
+package org.choongang.board.services.write;
+
+import lombok.RequiredArgsConstructor;
+import org.choongang.board.controllers.BoardSearch;
+import org.choongang.board.controllers.RequestBoardData;
+import org.choongang.board.entities.BoardData;
+import org.choongang.board.exceptions.BoardConfigNotFoundException;
+import org.choongang.board.mappers.BoardDataMapper;
+import org.choongang.global.config.annotations.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardWriteInfoService {
+
+}
