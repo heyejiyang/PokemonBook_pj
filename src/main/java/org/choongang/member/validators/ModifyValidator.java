fd0556633c40060f95b2dca902a2f44e079b5a0d
kth232 <kth7537@naver.com>
2024-07-01 12:13:51 +0900

    회원 정보 수정 오류 확인 완료!

--- src/main/java/org/choongang/member/validators/ModifyValidator.java
+++ src/main/java/org/choongang/member/validators/ModifyValidator.java
@@ -27,31 +27,23 @@ public class ModifyValidator implements Validator<RequestModify>, RequiredValida
         checkTrue(memberUtil.isLogin(), new UnAuthorizedException());
 
         /*필수 항목 검증 S*/
-//        String email = form.getEmail();
         String password = form.getPassword();
         String confirmPassword = form.getConfirmPassword();
         String userName = form.getUserName();
         int status = HttpServletResponse.SC_BAD_REQUEST;
 
-        /* 필수 항목 유효성 검사 S */
-//        checkRequired(email, new AlertException("이메일을 입력하세요", status));
-
-        // 이메일 형식 체크
-//        checkTrue(checkEmail(email), new AlertException("이메일 형식이 아닙니다.", status));
-
         if (password != null && !password.isBlank()) {
             checkRequired(password, new AlertException("비밀번호를 입력하세요.", status));
             checkRequired(confirmPassword, new AlertException("비밀번호를 확인하세요.", status));
 
+            /*비밀번호 자리수 체크*/
             checkTrue(password.length() >= 8, new AlertException("비밀번호는 8자리 이상 입력하세요.", status));
 
+            /*비밀번호 및 비밀번호 확인 일치 여부*/
             checkTrue(password.equals(confirmPassword), new AlertException("비밀번호가 일치하지 않습니다.", status));
         }
         checkRequired(userName, new AlertException("회원명을 입력하세요.", status));
         /* 필수 항목 유효성 검사 E */
 
-
-        // 이메일 중복 여부 체크 - 이미 가입된 회원인지 여부
-//        checkTrue(mapper.exists(email) == 0L, new AlertException("이미 가입된 이메일입니다.", status));
     }
 }
