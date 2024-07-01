1cbab734c65b58938206c4ec8979ea0a0193dffd
hyeon12 <seojh912_@naver.com>
2024-07-01 10:05:06 +0900

    modelmapper 추가

--- src/main/java/org/choongang/board/entities/BoardData.java
+++ src/main/java/org/choongang/board/entities/BoardData.java
@@ -1,12 +1,16 @@
 package org.choongang.board.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardData {
     private long num;
     private long seq;
--- src/main/java/org/choongang/board/services/BoardSaveService.java
+++ src/main/java/org/choongang/board/services/BoardSaveService.java
@@ -1,35 +1,49 @@
 package org.choongang.board.services;
 
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.choongang.board.controllers.RequestBoardData;
 import org.choongang.board.entities.BoardData;
 import org.choongang.board.mappers.BoardDataMapper;
 import org.choongang.board.validators.BoardSaveValidator;
 import org.choongang.global.config.annotations.Service;
-import org.choongang.global.exceptions.AlertException;
+import org.choongang.global.config.containers.BeanContainer;
+import org.choongang.member.MemberUtil;
+import org.modelmapper.ModelMapper;
 
 @Service
 @RequiredArgsConstructor
 //게시글 작성, 수정
 public class BoardSaveService {
+
+    private final MemberUtil memberUtil;
     private final BoardDataMapper mapper;
     private final BoardSaveValidator validator; //권한 체크
 
-    public void process(RequestBoardData data){
-//        validator.check(data);
-//
-//        BoardData boardData = BoardData.builder()
-//            .bId(data.getBId())
-//            .gId(data.getBId())
-//            .poster(data.getPoster())
-//            .subject(data.getSubject())
-//            .content(data.getContent())
-//            .guestPassword(data.getGuestPassword())
-//            .category(data.getCategory())
-//            .build();
-//
+    public void save(RequestBoardData form){
+        validator.check(form);
 
+        String mode = form.getMode();
+        mode = mode == null || mode.isBlank()? "write" : mode;
+
+        BoardData data = new ModelMapper().map(form, BoardData.class);
+
+        if(mode.equals("update")){ //수정
+
+
+        } else { // 등록
+            //현재 로그인한 회원 번호
+            if(memberUtil.isLogin()) {
+                data.setMemberSeq(memberUtil.getMember().getUserNo());
+            }
+
+            //작성자 환경 정보
+            HttpServletRequest request = BeanContainer.getInstance().getBean(HttpServletRequest.class);
+            String ua = request.getHeader("User-Agent"); //사용자 브라우저 정보 -> getHeader 통해 가져옴
+            String ip = request.getRemoteAddr(); //작성자 IP 정보
+            data.setUa(ua);
+            data.setIp(ip);
+        }
 
     }
 
