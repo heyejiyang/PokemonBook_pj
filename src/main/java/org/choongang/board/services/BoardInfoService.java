ac76ea2838c85c238af86224f990686d5e16d9a7
heyejiyang <xoxo99312@naver.com>
2024-07-01 16:52:14 +0900

    게시판 write, update기능 view 출력

--- src/main/java/org/choongang/board/services/BoardInfoService.java
+++ src/main/java/org/choongang/board/services/BoardInfoService.java
@@ -3,7 +3,6 @@ package org.choongang.board.services;
 import lombok.RequiredArgsConstructor;
 import org.choongang.board.controllers.BoardSearch;
 import org.choongang.board.controllers.RequestBoardData;
-import org.choongang.board.entities.Board;
 import org.choongang.board.entities.BoardData;
 import org.choongang.board.exceptions.BoardNotFoundException;
 import org.choongang.board.mappers.BoardDataMapper;
@@ -20,32 +19,41 @@ public class BoardInfoService {
 
     /**
      * 게시글 번호로 게시글 조회
-     *  - 쓰기, 수정, 목록, 보기 권한 데이터 추가
-     *  - 댓글 목록 데이터 추가
-     *  - 게시글에 첨부한 이미지 또는 파일 목록
+     *      - 쓰기, 수정, 목록, 보기 권한 데이터 추가
+     *      - 댓글 목록 데이터 추가
+     *      - 게시글에 첨부한 이미지 또는 파일 목록
+     *
      * @param seq
      * @return
      */
-    public Optional<BoardData> get(long seq){
+    public Optional<BoardData> get(long seq) {
         BoardData data = mapper.get(seq);
+
 
         return Optional.ofNullable(data);
     }
 
-    public RequestBoardData getForm(long seq){
-        BoardData data = get(seq).orElseThrow(BoardNotFoundException::new);
-        RequestBoardData form = new ModelMapper().map(data,RequestBoardData.class);
 
+    public RequestBoardData getForm(long seq) {
+        BoardData data = get(seq).orElseThrow(BoardNotFoundException::new);
+        return getForm(data);
+    }
+
+    public RequestBoardData getForm(BoardData data){
+        RequestBoardData form = new ModelMapper().map(data, RequestBoardData.class);
+        form.setMode("update");
         return form;
     }
 
-    public ListData<BoardData> getList(BoardSearch search){
+    public ListData<BoardData> getList(BoardSearch search) {
+
         return null;
     }
 
-    public ListData<BoardData> getList(String bId, BoardSearch search){
+    public ListData<BoardData> getList(String bId, BoardSearch search) {
         search.setBId(bId);
 
         return getList(search);
     }
+
 }
--- src/main/webapp/WEB-INF/templates/board/view.jsp
+++ src/main/webapp/WEB-INF/templates/board/view.jsp
@@ -2,7 +2,25 @@
 <%@ page contentType="text/html; charset=UTF-8"%>
 <%@ taglib prefix="c" uri="jakarta.tags.core" %>
 <%@taglib prefix="layout" tagdir="/WEB-INF/tags/layouts" %>
+<%@ taglib prefix="util" tagdir="/WEB-INF/tags/utils" %>
 
-<layout:main>
-
+<layout:main title="${board.BName}">
+    <section class="layout-width">
+        <jsp:include page="_header.jsp"/>
+        <div class="subject">
+            ${data.subject}
+        </div>
+        <div class="poster-info">
+            <div class="left">
+                ${data.poster}
+                (${data.memberSeq > 0 ? data.email : '비회원'})
+            </div>
+            <div class="right">
+                IP: ${data.ip} / DATE: <util:formatDate value="${data.regDt}" pattern="yyyy.MM.dd HH:mm"></util:formatDate>
+            </div>
+        </div>
+        <div class="content">
+            ${data.content}
+        </div>
+    </section>
 </layout:main>
