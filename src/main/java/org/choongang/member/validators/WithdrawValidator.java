83cbc1db61b9a4a5a944237cfdc8dd4824ad8fb2
kth232 <kth7537@naver.com>
2024-07-03 18:58:42 +0900

    회원 탙퇴 기능 구현 오류 수정 중

--- src/main/java/org/choongang/member/validators/WithdrawValidator.java
+++ src/main/java/org/choongang/member/validators/WithdrawValidator.java
@@ -26,14 +26,13 @@ public class WithdrawValidator implements Validator<RequestWithdraw>, RequiredVa
         checkTrue(memberUtil.isLogin(), new UnAuthorizedException());
 
         /*필수 항목 검증 S*/
-        if (password != null && !password.isBlank()) {
-            checkRequired(password, new AlertException("비밀번호를 입력하세요.", status));
-            checkRequired(confirmPassword, new AlertException("비밀번호를 확인하세요.", status));
+
+        checkRequired(password, new AlertException("비밀번호를 입력하세요.", status));
+        checkRequired(confirmPassword, new AlertException("비밀번호를 확인하세요.", status));
 
 
-            /*비밀번호 및 비밀번호 확인 일치 여부*/
-            checkTrue(password.equals(confirmPassword), new AlertException("비밀번호가 일치하지 않습니다.", status));
-        }
+        /*비밀번호 및 비밀번호 확인 일치 여부*/
+        checkTrue(password.equals(confirmPassword), new AlertException("비밀번호가 일치하지 않습니다.", status));
         /* 필수 항목 유효성 검사 E */
     }
 
