7d7b112f40b31c08f02743973f9f72fb39aabb44
kth232 <kth7537@naver.com>
2024-07-04 14:46:55 +0900

    작은 수정..

--- src/main/java/org/choongang/admin/advices/AdminControllerAdvice.java
+++ src/main/java/org/choongang/admin/advices/AdminControllerAdvice.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.choongang.global.Interceptor;
 import org.choongang.global.config.annotations.ControllerAdvice;
 import org.choongang.global.config.annotations.ModelAttribute;
+import org.choongang.global.exceptions.UnAuthorizedException;
 import org.choongang.member.MemberUtil;
 
 import java.util.ArrayList;
@@ -25,7 +26,7 @@ public class AdminControllerAdvice implements Interceptor {
     public boolean preHandle() {
 
         if (!memberUtil.isAdmin()) {
-            //throw new UnAuthorizedException();
+            throw new UnAuthorizedException();
         }
 
         return true;
